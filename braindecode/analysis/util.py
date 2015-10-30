f9424a21cb19ac29423fd427954381c327f7bd5e
Robin Tibor Schirrmeister <robintibor@gmail.com>
2015-10-30 13:51:33 +0100

    added n parameter to bps and freqs

--- braindecode/analysis/util.py
+++ braindecode/analysis/util.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-def bps_and_freqs(weights, axis=1, sampling_rate=150.0):
-    bps = np.abs(np.fft.rfft(weights, axis=axis))
-    freq_bins = np.fft.rfftfreq(weights.shape[axis], d=1.0/sampling_rate)
+def bps_and_freqs(weights, axis=1, sampling_rate=150.0, n=None):
+    bps = np.abs(np.fft.rfft(weights, axis=axis, n=n))
+    n_samples = n
+    if n_samples is None:
+        n_samples = weights.shape[axis]
+    freq_bins = np.fft.rfftfreq(n_samples, d=1.0/sampling_rate)
     return bps, freq_bins
