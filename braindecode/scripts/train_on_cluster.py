edf140ac7c645de87d7edd8d010bb9dc1709301c
Robin Tibor Schirrmeister <robintibor@gmail.com>
2015-11-11 16:22:52 +0100

    ability to specify hostname when training on cluster

--- braindecode/scripts/train_on_cluster.py
+++ braindecode/scripts/train_on_cluster.py
@@ -5,11 +5,20 @@ import subprocess
 
 if __name__ == "__main__":
     queue = sys.argv[1]
-    assert queue == 'tf' or queue == 'rz', ("only know rz and tf queues, "
-    "not: " + queue)
-    job_args = sys.argv[2:]
+    assert queue == 'tf' or queue == 'rz' or queue == 'test', ("only know "
+        "rz and tf and test queues, not: " + queue)
+    if sys.argv[2].startswith('metagpu'):
+        hostname = sys.argv[2]
+        print("Running on {:s}".format(hostname))
+        job_args = sys.argv[3:]
+    else:
+        job_args = sys.argv[2:]
+        hostname = None
     job_filepath = generate_cluster_job(job_args)
-    command = "qsub -q meta_gpu-{:s}.q {:s}".format(queue, job_filepath)
+    if hostname is not None:
+        command = "qsub -l hostname={:s} -q meta_gpu-{:s}.q {:s}".format(hostname, queue, job_filepath)
+    else:
+        command = "qsub -q meta_gpu-{:s}.q {:s}".format(queue, job_filepath)
     print("Running:\n" + command)
     subprocess.call([command],shell=True)
     
