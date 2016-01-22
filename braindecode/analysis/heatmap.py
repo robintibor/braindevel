e0e0920543c7646963033a283cf585ae3ab12bc6
Robin Tibor Schirrmeister <robintibor@gmail.com>
2016-01-22 17:16:17 +0100

    z_plus, more tests etc

--- braindecode/analysis/heatmap.py
+++ braindecode/analysis/heatmap.py
@@ -3,25 +3,54 @@ import theano.tensor as T
 import numpy as np
 import theano
 import lasagne
+pool_fun = None
 
-inputs = T.ftensor3()
-output = downsample.max_pool_2d(inputs,ds=(2,2),mode='sum')#, ignore_border=True)
-pool_fun = theano.function([inputs], output)
-
-def back_relevance_dense_layer(out_relevances, in_activations, weights):
-    """In_activations not used atm. would be needed for z or z+ rule"""
-    squared_weights = weights * weights
-    denominators_per_j = np.sum(squared_weights, axis=0)  # + np.maximum(softmax_biases, 0)
-    scaled_weights = squared_weights / denominators_per_j
-    input_relevances = np.sum(scaled_weights * out_relevances, axis=1)
+def back_relevance_dense_layer(out_relevances, in_activations, weights, rule):
+    assert rule in ['w_sqr', 'z_plus']
+    # for tests where i put int numbers
+    weights = np.array(weights, dtype=np.float32)
+    out_relevances = np.array(out_relevances, dtype=np.float32)
+    in_activations = np.array(in_activations, dtype=np.float32)
+    # weights are features x output_units => input_units x output_units
+    # in_activations are input_units
+    if rule == 'w_sqr':
+        W_adapted = weights * weights
+    if rule == 'z_plus':
+        if in_activations.ndim > 1:
+            in_activations = in_activations.flatten()
+        W_plus = weights * (weights > 0)
+        W_adapted = W_plus * in_activations[:, np.newaxis]
+    W_scaled = W_adapted / np.sum(W_adapted, axis=0)
+    W_scaled[:,np.sum(W_adapted, axis=0) == 0] = 1.0 / W_adapted.shape[0]
+    input_relevances = W_scaled * out_relevances
+    input_relevances = np.sum(input_relevances, axis=1)
     return input_relevances
 
-def back_relevance_conv(out_relevances, in_activations, conv_weights, rule):
-    #... do it differently go back form out relevances since they have to be redistributed
-    conv_weights = conv_weights.astype(np.float32)
-    out_relevances = out_relevances.astype(np.float32)
-    in_activations = in_activations.astype(np.float32)
+
+def back_relevance_dense_layer_as_in_paper(out_relevances, in_activations, weights, rule):
+    """Just for reference.. leads to same results (except not checking for NaNs yet)..."""
     assert rule in ['w_sqr', 'z_plus']
+    # for tests where i put int numbers
+    weights = np.array(weights, dtype=np.float32)
+    out_relevances = np.array(out_relevances, dtype=np.float32)
+    in_activations = np.array(in_activations, dtype=np.float32)
+    # weights are features x output_units => input_units x output_units
+    # in_activations are input_units
+    if rule == 'w_sqr':
+        W_adapted = weights * weights
+        N = W_adapted / np.sum(W_adapted, axis=0)
+        return np.dot(N, out_relevances)
+    if rule == 'z_plus':
+        V = weights * (weights > 0)
+        Z = np.dot(V.T, in_activations)
+        return in_activations * np.dot(V, (out_relevances / Z))
+
+def back_relevance_conv(out_relevances, in_activations, conv_weights, rule):
+    assert rule in ['w_sqr', 'z_plus']
+    # for tests where i put int numbers
+    conv_weights = np.array(conv_weights, dtype=np.float32)
+    out_relevances = np.array(out_relevances, dtype=np.float32)
+    in_activations = np.array(in_activations, dtype=np.float32)
     kernel_size = conv_weights.shape[2:]
     in_relevances = np.zeros(in_activations.shape)
     for out_filt in xrange(out_relevances.shape[0]):
@@ -44,6 +73,12 @@ def back_relevance_conv(out_relevances, in_activations, conv_weights, rule):
     return in_relevances
 
 def back_relevance_pool(out_relevances, in_activations, pool_size, pool_stride):
+    # FIrst create pool function if it doesn't exist
+    global pool_fun
+    if pool_fun is None:
+        inputs = T.ftensor3()
+        output = downsample.max_pool_2d(inputs,ds=(2,2),mode='sum')#, ignore_border=True)
+        pool_fun = theano.function([inputs], output)
     assert pool_size == pool_stride, "At the moment only support pool size and stride same"
     # have to upsample relevances and compute sums of activations per pool region
     upsampled_relevances = np.repeat(np.repeat(out_relevances,pool_size[0],  axis=1), 
@@ -57,17 +92,21 @@ def back_relevance_pool(out_relevances, in_activations, pool_size, pool_stride):
     in_relevances = scaled_activations * upsampled_relevances
     return in_relevances
 
-def compute_heatmap(out_relevances, all_activations_per_layer, all_layers):
+def compute_heatmap(out_relevances, all_activations_per_layer, all_layers, 
+        all_rules):
+    for rule in all_rules:
+        assert rule in ['w_sqr', 'z_plus', None]
     # stop before first layer...as it should be input layer...
     # and we alwasys need activations from before
     for i_layer in xrange(len(all_layers)-1, 0,-1):
         # We have out relevance for that layer, now 
         layer = all_layers[i_layer]
         in_activations = all_activations_per_layer[i_layer-1]
+        rule = all_rules[i_layer]
         if isinstance(layer, lasagne.layers.DenseLayer):
             dense_weights = layer.W.get_value()
             out_relevances = back_relevance_dense_layer(out_relevances,
-                 in_activations, dense_weights)
+                 in_activations, dense_weights, rule)
         elif hasattr(layer, 'pool_size'):
             assert layer.stride == layer.pool_size, (
                 "Only works with stride equal size at the moment")
@@ -75,7 +114,8 @@ def compute_heatmap(out_relevances, all_activations_per_layer, all_layers):
                  pool_size=layer.pool_size, pool_stride=layer.stride)
         elif hasattr(layer, 'filter_size'):
             conv_weights = layer.W.get_value()
-            out_relevances = back_relevance_conv(out_relevances, in_activations,conv_weights)
+            out_relevances = back_relevance_conv(out_relevances, in_activations,
+                conv_weights, rule)
         else:
             raise ValueError("Trying to propagate through unknown layer")
         if out_relevances.shape != in_activations.shape:
--- braindecode/test/unit_tests/analysis/test_heatmap.py
+++ braindecode/test/unit_tests/analysis/test_heatmap.py
@@ -1,7 +1,8 @@
 import numpy as np
-from braindecode.analysis.heatmap import back_relevance_conv
+from braindecode.analysis.heatmap import (back_relevance_conv,
+    back_relevance_dense_layer)
 
-def test_w_sqr_conv():
+def test_conv_w_sqr():
     out_relevances = np.array([[[1,0,0]]]).astype(np.float32)
     conv_weights = np.array([[[[1,0]]]])[:,:,::-1,::-1]
     in_relevances = back_relevance_conv(out_relevances, np.zeros((1,1,4)), conv_weights, 'w_sqr')
@@ -24,7 +25,18 @@ def test_w_sqr_conv():
     in_relevances = back_relevance_conv(out_relevances, np.zeros((1,1,4)), conv_weights, 'w_sqr')
     assert np.array_equal([[[ 0.5,  0.5,  0.,  0.]]], in_relevances)
     
-def test_z_plus():
+    assert np.allclose([ 1.57243109,  4.92130327,  0.50626564],
+                   back_relevance_dense_layer(np.array([2,1,0,3,1]),
+                           np.array([1,0,3]),
+                           np.array([[0,1,2,3,4], [9,3,4,-5,1],[0,0,5,-2,2]]), 
+                          'w_sqr'))
+    assert np.allclose([1.65684497,  4.97152281,  1.37163186], 
+        back_relevance_dense_layer(np.array([2,1,1,3,1]),
+                           np.array([1,4,3]),
+                           np.array([[1,1,2,3,4], [9,3,4,-5,1],[1,2,5,-2,2]]), 
+                          'w_sqr'))
+    
+def test_conv_z_plus():
     out_relevances = np.array([[[1,0,0]]]).astype(np.float32)
     conv_weights = np.array([[[[1,2]]]])[:,:,::-1,::-1]
     in_relevances = back_relevance_conv(out_relevances, np.ones((1,1,4)), conv_weights, 'z_plus')
@@ -38,3 +50,24 @@ def test_z_plus():
     conv_weights = np.array([[[[1,-2]]]])[:,:,::-1,::-1]
     in_relevances = back_relevance_conv(out_relevances, np.ones((1,1,4)), conv_weights, 'z_plus')
     assert np.allclose([[[ 1,0,  0.,  0.]]], in_relevances)
+    in_activations = [[[1,2,6]]]
+    out_relevances = [[[3,4]]]
+    weights = np.array([[[[2,3]]]])[:,:,::-1,::-1]
+    in_relevances = back_relevance_conv(out_relevances, in_activations,weights, rule='z_plus')
+    assert np.allclose([[[3/4.0, 131/44.0, 36/11.0]]], in_relevances)
+    
+def test_dense_z_plus():
+    out_relevances = np.array([0,2,1], dtype=np.float32)
+    weights = np.array([[1,1,1], [2,2.25,0]], dtype=np.float32)
+    in_activations = np.array([1,4], dtype=np.float32)
+    in_relevances = back_relevance_dense_layer(out_relevances,in_activations,weights,'z_plus')
+    assert np.allclose([1.2,1.8], in_relevances)
+    assert np.allclose([ 3.44895196,  3.20214176,  1.3489064 ],
+                   back_relevance_dense_layer(np.array([2,1,1,3,1]),
+                           np.array([1,4,3]),
+                           np.array([[1,1,2,3,4], [9,3,4,-5,1],[1,2,5,-2,2]]), 
+                          'z_plus'))
+    assert np.allclose([ 5.06666708,  0.66666669,  1.26666665], back_relevance_dense_layer(np.array([2,1,0,3,1]),
+                           np.array([1,0,3]),
+                           np.array([[0,1,2,3,4], [9,3,4,-5,1],[0,0,5,-2,2]]), 
+                          'z_plus'))
