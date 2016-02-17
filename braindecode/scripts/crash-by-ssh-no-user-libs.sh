94a9ad859680007fa4fbfe99a4ed4ad412df0b31
Robin Tibor Schirrmeister <robintibor@gmail.com>
2016-02-17 12:01:32 +0100

    crash without user libs

---
+++ braindecode/scripts/crash-by-ssh-no-user-libs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+JOB_NUMBER=$1
+
+
+LD_LIBRARY_PATH=/usr/local/cudnn-7.0-v3/lib64:$LD_LIBRARY_PATH
+LIBRARY_PATH=/usr/local/cudnn-7.0-v3/lib64:$LIBRARY_PATH
+CPATH=/usr/local/cudnn-7.0-v3/include:$CPATH
+
+GPU_ID=0
+
+
+CUDA_VISIBLE_DEVICES=$JOB_NUMBER
+PATH=/usr/local/cuda-7.5/bin/:/usr/bin/:$PATH  
+THEANO_FLAGS=floatX=float32,device=gpu0,compiledir=/tmp/sudo_theano_crash$JOB_NUMBER/
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH LIBRARY_PATH=$LIBRARY_PATH CPATH=$CPATH CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES PATH=$PATH THEANO_FLAGS=$THEANO_FLAGS python ./crash_script_self_contained.py
