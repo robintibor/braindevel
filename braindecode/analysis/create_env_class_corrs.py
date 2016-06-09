Fixed batch modifier test to use phase, fixed off-by-one-sample error in computing trial acts

braindecode/analysis/create_env_class_corrs.py
@@ -30,10 +30,12 @@ def create_env_class_corrs(folder, params,start,stop):
         log.info("Running {:s} ({:d} of {:d})".format(base_name,
             i_exp + start + 1, stop))
         exp, model = load_exp_and_model(base_name)
-        exp.dataset.load()
+        exp.dataset.load()    
+        train_set = exp.dataset_provider.get_train_merged_valid_test(exp.dataset)['train']
+
         trial_env = load_trial_env(base_name + '.env.npy',
                model, i_layer=26, # 26 is last max-pool i think 
-               train_set=exp.dataset.train_set,
+               train_set=train_set,
               n_inputs_per_trial=2)
         topo_corr = compute_env_class_corr(exp, trial_env)
         rand_model = create_experiment(base_name + '.yaml').final_layer
@@ -54,7 +56,8 @@ def compute_env_class_corr(exp, trial_env):
         check_trial_lengths_equal=True,
         input_time_length=exp.iterator.input_time_length)
     assert len(i_trial_ends) == trial_env.shape[1]
-    y_signal = [exp.dataset.train_set.y[i_start:i_end]
+    # +1 as i_end is inclusive
+    y_signal = [exp.dataset.train_set.y[i_start:i_end+1]
         for i_start, i_end in zip(i_trial_starts, i_trial_ends)]
     y_signal = np.array(y_signal).transpose(0,2,1)
     assert y_signal.shape[2] == trial_env.shape[3]

braindecode/analysis/envelopes.py
@@ -41,7 +41,8 @@ def transform_to_trial_env(env, model, i_layer, train_set,
     trial_starts, trial_ends = compute_trial_start_end_samples(train_set.y)
     assert len(np.unique(trial_ends - trial_starts)) == 1
     n_trials = len(trial_starts)
-    n_trial_len = np.unique(trial_ends - trial_starts)[0]
+    # +1 as trial end is inclusive
+    n_trial_len = np.unique(trial_ends - trial_starts)[0] + 1
     # Afterwards env is list empty lists(!!)
     n_sample_preds = get_n_sample_preds(model)
     trial_env = get_meaned_trial_env(env, field_size=field_size, 
@@ -90,7 +91,8 @@ def get_meaned_trial_env(env, field_size, n_trials, n_inputs_per_trial,
     for fb_env in meaned_env:
         fb_envs_per_trial = fb_env.reshape(n_trials,n_inputs_per_trial,
             fb_env.shape[1], fb_env.shape[2], fb_env.shape[3])
-        trial_env = transform_to_trial_acts(fb_envs_per_trial, [n_inputs_per_trial] * n_trials,
+        trial_env = transform_to_trial_acts(fb_envs_per_trial,
+            [n_inputs_per_trial] * n_trials,
                                             n_sample_preds=n_sample_preds,
                                             n_trial_len=n_trial_len)
         all_trial_envs.append(trial_env)

braindecode/test/unit_tests/datahandling/test_batch_modifier.py
@@ -12,6 +12,6 @@ def test_phase_equal_after_bandpower_mean():
     target_arr[:,2] = targets == 2
     target_arr[:,3] = targets == 3
     mod_inputs, mod_targets = BandpowerMeaner().process(inputs, target_arr)
-    assert np.allclose(np.imag(np.fft.rfft(inputs, axis=2)),
-            np.imag(np.fft.rfft(mod_inputs, axis=2)), rtol=1e-4, atol=1e-5)
+    assert np.allclose(np.angle(np.fft.rfft(inputs, axis=2)),
+            np.angle(np.fft.rfft(mod_inputs, axis=2)), rtol=1e-4, atol=1e-5)
     assert np.array_equal(target_arr, mod_targets)                          

braindecode/veganlasagne/layer_util.py
@@ -24,14 +24,15 @@ def compute_trial_acts(model, i_layer, iterator, train_set):
     
     Returns
     -------
-    trial_acts: 4darray of float
-        Activations per trial per sample.
+    trial_acts: 3darray of float
+        Activations per trial per sample. #trialx#channelx#sample
     """
     # compute number of inputs per trial
     i_trial_starts, i_trial_ends =  compute_trial_start_end_samples(
                 train_set.y, check_trial_lengths_equal=True,
                 input_time_length=iterator.input_time_length)
-    trial_len = i_trial_ends[0] - i_trial_starts[0]
+    # +1 since trial ends is inclusive
+    trial_len = i_trial_ends[0] - i_trial_starts[0] + 1
     n_inputs_per_trial = int(np.ceil(trial_len / float(iterator.n_sample_preds)))
     log.info("Create theano function...")
     all_layers = lasagne.layers.get_all_layers(model)